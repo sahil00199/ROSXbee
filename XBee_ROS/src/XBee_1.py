b3bfcfc4b880eda10f658b205ab6fdb6d34d4786
Agrim9 <agrim9gupta@gmail.com>
2017-01-21 01:02:57 +0530

    XBee RX test

---
+++ XBee_ROS/src/XBee_1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import time
+import serial
+import rospy
+from std_msgs.msg import String
+from multiprocessing import Process
+
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+    )
+
+def send():
+    hello_str = "hello %s" % rospy.get_time()
+    pub.publish(hello_str)
+        
+        # Python 3 users
+        # input = input(">> ")
+    rospy.loginfo("tx!")
+    ser.write(hello_str)
+
+def receive():
+    out = ''
+    out += ser.read(1)
+    time.sleep(1)
+    rospy.loginfo("rx: ")
+    rospy.loginfo(out) 
+    pub1.publish(out)
+
+
+if __name__ == '__main__':
+
+    # configure the serial connections (the parameters differs on the device you are connecting to)
+  ser.isOpen()
+  rospy.init_node('XBee_1')
+  pub=rospy.Publisher('XBee_1_TX', String, queue_size=10)
+  pub1=rospy.Publisher('XBee_1_RX', String, queue_size=10)
+  rate=rospy.Rate(10)
+  p = Process(target=send, args=())
+  q = Process(target=receive, args=())    
+   while not rospy.is_shutdown():
+      p.start()
+      q.start()
+      rate.sleep()
+  
+  p.join()
+  q.join()
+      
