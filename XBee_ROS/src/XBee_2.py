3638cb792f272765784b1cd6f3589cf0e9b19de7
Sahil <sahilshah00199@gmail.com>
2017-01-25 23:47:07 +0530

    First Working Version

--- XBee_ROS/src/XBee_2.py
+++ XBee_ROS/src/XBee_2.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-
 import time
 import serial
 import rospy
+import thread
 from std_msgs.msg import String
-from multiprocessing import Process
+
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
@@ -15,38 +14,30 @@ ser = serial.Serial(
     )
 
 def send():
-    hello_str = "hello %s" % rospy.get_time()
-    pub.publish(hello_str)
-        
+    input = 'hii'
         # Python 3 users
         # input = input(">> ")
-    rospy.loginfo("tx!")
-    ser.write(hello_str)
+    rospy.loginfo("Txed The signal")
+    pub_tx.publish(input)
+    ser.write(input + '\r\n')
 
 def receive():
     out = ''
     out += ser.read(1)
     time.sleep(1)
-    rospy.loginfo("rx: ")
-    rospy.loginfo(out) 
-    pub1.publish(out)
+    pub_rx.publish(out)
+    rospy.loginfo("Rcvd Mssg",out)
 
 
 if __name__ == '__main__':
 
     # configure the serial connections (the parameters differs on the device you are connecting to)
-  ser.isOpen()
-  rospy.init_node('XBee_2')
-  pub=rospy.Publisher('XBee_2_TX', String, queue_size=10)
-  pub1=rospy.Publisher('XBee_2_RX', String, queue_size=10)
-  rate=rospy.Rate(10)
-  p = Process(target=send, args=())
-  q = Process(target=receive, args=())    
-  while not rospy.is_shutdown():
-      p.start()
-      q.start()
-      p.join()
-      q.join()
-      rate.sleep()
-  
-      
+    ser.isOpen()
+    rospy.init_node('XBee_2', anonymous=true)
+    pub_tx=rospy.Publisher('XBee_2_TX', String, queue_size=10)
+    pub_rx=rospy.Publisher('XBee_2_RX', String, queue_size=10)
+    rate=rospy.Rate(0.1)
+    thread.start_new_thread(receive,())
+    while not rospy.is_shutdown():
+        thread.start_new_thread(send,())        
+        rate.sleep()    
